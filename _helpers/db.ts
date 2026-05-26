3bc6ce2c3249b2581981fc490c6ab6dc0af137bc
dev082423 <barzillaidaves@gmail.com>
2026-05-26 23:30:04 +0800

    feat: make a quick fix for env render

--- _helpers/db.ts
+++ _helpers/db.ts
@@ -10,14 +10,23 @@ export default db;
 initialize();
 
 async function initialize() {
-  const { host, port, user, password, database } = config.database;
+  const host = process.env.DB_HOST || config.database.host;
+  const port = Number(process.env.DB_PORT) || config.database.port;
+  const user = process.env.DB_USER || config.database.user;
+  const password = process.env.DB_PASS || config.database.password;
+  const database = process.env.DB_NAME || config.database.database;
+
   const connection = await mysql.createConnection({ host, port, user, password });
 
   // Create DB if it doesn't exist
   await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
 
   // Connect to DB
-  const sequelize = new Sequelize(database, user, password, { dialect: 'mysql' });
+  const sequelize = new Sequelize(database, user, password, { 
+    host,
+    port,
+    dialect: 'mysql' 
+  });
 
   // Init models
   db.Account = accountModel(sequelize);
